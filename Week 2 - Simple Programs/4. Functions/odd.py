3e2eaecd29d0bc37ab83a3a5d568702c5c13be31
Yann ILAS <yann.ilas@gmail.com>
2017-01-29 14:47:24 +0100

    init version of odd.py

---
+++ Week 2 - Simple Programs/4. Functions/odd.py
@@ -0,0 +1,12 @@
+def odd(x):
+    '''
+    x: int or float.
+    returns: True if x is odd, False otherwise
+    '''
+    return bool(x % 2)
+
+print odd(2)
+print odd(3)
+print odd(40)
+print odd(41)
+
